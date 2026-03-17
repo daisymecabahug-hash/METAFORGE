4ca77c7f70ac1a36e6bb6e5e1881c317aa716112
daisymecabahug-hash <daisymecabahug@gmail.com>
2026-03-17 23:27:48 +0800

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -20,4 +20,23 @@
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
   const analytics = getAnalytics(app);
-</script>
+</script>// firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-analytics.js";
+
+// Your Firebase config
+const firebaseConfig = {
+  apiKey: "AIzaSyADMA-luLhqHQCNNAsKZlvuPXRlY8_Dicw",
+  authDomain: "metaforge-71c49.firebaseapp.com",
+  projectId: "metaforge-71c49",
+  storageBucket: "metaforge-71c49.firebasestorage.app",
+  messagingSenderId: "459212032201",
+  appId: "1:459212032201:web:15e06c515f4aaf8fb1e518",
+  measurementId: "G-RR4SHEL0BR"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const analytics = getAnalytics(app);
+
+console.log("Firebase initialized", app);
